777fb7b1ce647d04e6f696ac9d539f15896635c0
David Fernandes <davidsf@pantherify.com>
2022-05-30 12:13:23 +0000

    tested & working with reports

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from concurrent import futures
 import dotenv
 import grpc
 import jsonpickle
+import json
 
 from cf.cf_pb2_grpc import CFModuleServicer, add_CFModuleServicer_to_server
 from cf.cf_pb2 import CharacterizationInput, CharacterizationOutput, CharacterizationSourceOutput, PlatformOnlyInput, ConvertSinkOutput, ConvertSourceInput, ConvertSourceOutput, \
@@ -36,38 +37,38 @@ class CFModule(CFModuleServicer):
 
         result = convert_sinks(in_var=in_var, kb=kb)
         return ConvertSinkOutput(
-            all_sinks_info=jsonpickle.encode(
-                result["all_sinks_info"], unpicklable=False),
-            n_demand_list=jsonpickle.encode(
-                result["n_demand_list"], unpicklable=False),
-            teo_demand_factor_group=jsonpickle.encode(
-                result["teo_demand_factor_group"], unpicklable=False),
+            all_sinks_info=json.dumps(
+                result["all_sinks_info"]),
+            n_demand_list=json.dumps(
+                result["n_demand_list"]),
+            teo_demand_factor_group=json.dumps(
+                result["teo_demand_factor_group"]),
         )
 
     def convert_source(self, request: ConvertSourceInput, context):
         in_var = {
             "platform": jsonpickle.decode(request.platform),
-            "gis-module": jsonpickle.decode(request.gis_module),
-            "cf-module": jsonpickle.decode(request.cf_module)
+            "gis_module": jsonpickle.decode(request.gis_module),
+            "cf_module": jsonpickle.decode(request.cf_module)
         }
 
         result = convert_sources(in_var=in_var, kb=kb)
         return ConvertSourceOutput(
-            all_sources_info=jsonpickle.encode(
-                result['all_sources_info'], unpicklable=False),
-            teo_string=jsonpickle.encode(
-                result['teo_string'], unpicklable=False),
-            input_fuel=jsonpickle.encode(
-                result['input_fuel'], unpicklable=False),
-            output_fuel=jsonpickle.encode(
-                result['output_fuel'], unpicklable=False),
-            output=jsonpickle.encode(result['output'], unpicklable=False),
-            input=jsonpickle.encode(result['input'], unpicklable=False),
-            n_supply_list=jsonpickle.encode(
-                result['n_supply_list'], unpicklable=False),
-            teo_capacity_factor_group=jsonpickle.encode(
-                result['teo_capacity_factor_group'], unpicklable=False),
-            teo_dhn=jsonpickle.encode(result['teo_dhn'], unpicklable=False),
+            all_sources_info=json.dumps(
+                result['all_sources_info']),
+            teo_string=json.dumps(
+                result['teo_string']),
+            input_fuel=json.dumps(
+                result['input_fuel']),
+            output_fuel=json.dumps(
+                result['output_fuel']),
+            output=json.dumps(result['output']),
+            input=json.dumps(result['input']),
+            n_supply_list=json.dumps(
+                result['n_supply_list']),
+            teo_capacity_factor_group=json.dumps(
+                result['teo_capacity_factor_group']),
+            teo_dhn=json.dumps(result['teo_dhn']),
         )
 
     def char_greenhouse(self, request: PlatformOnlyInput, context):
@@ -76,8 +77,8 @@ class CFModule(CFModuleServicer):
         }
         result = greenhouse(in_var=in_var)
         return CharacterizationSinkOutput(
-            hot_stream=jsonpickle.encode(
-                result['hot_stream'], unpicklable=False),
+            hot_stream=json.dumps(
+                result['hot_stream']),
         )
 
     def char_building(self, request: PlatformOnlyInput, context):
@@ -86,10 +87,8 @@ class CFModule(CFModuleServicer):
         }
         result = building(in_var=in_var, kb=kb)
         return CharacterizationSinkOutput(
-            hot_stream=jsonpickle.encode(
-                result['hot_stream'], unpicklable=False),
-            cold_stream=jsonpickle.encode(
-                result['cold_stream'], unpicklable=False),
+            hot_stream=json.dumps(result['hot_stream']),
+            cold_stream=json.dumps(result['cold_stream']),
         )
 
     def convert_orc(self, request: PlatformOnlyInput, context):
@@ -98,8 +97,8 @@ class CFModule(CFModuleServicer):
         }
         result = convert_orc(in_var=in_var, kb=kb)
         return ConvertOrcOutput(
-            best_options=jsonpickle.encode(
-                result['best_options'], unpicklable=False),
+            best_options=json.dumps(result['best_options']),
+            report=result['report']
         )
 
     def convert_pinch(self, request: PlatformOnlyInput, context):
@@ -108,12 +107,11 @@ class CFModule(CFModuleServicer):
         }
         result = convert_pinch(in_var=in_var, kb=kb)
         return ConvertPinchOutput(
-            co2_optimization=jsonpickle.encode(
-                result['co2_optimization'], unpicklable=False),
-            energy_recovered_optimization=jsonpickle.encode(
-                result['energy_recovered_optimization'], unpicklable=False),
-            energy_investment_optimization=jsonpickle.encode(
-                result['energy_investment_optimization'], unpicklable=False),
+            co2_optimization=json.dumps(result['co2_optimization']),
+            energy_recovered_optimization=json.dumps(
+                result['energy_recovered_optimization']),
+            energy_investment_optimization=json.dumps(
+                result['energy_investment_optimization']),
         )
 
     def char_simple(self, request: PlatformOnlyInput, context):
@@ -122,16 +120,16 @@ class CFModule(CFModuleServicer):
         }
         result = simple_user(in_var=in_var)
         return CharacterizationSourceOutput(
-            streams=jsonpickle.encode(result['streams'], unpicklable=False),
+            streams=json.dumps(result['streams']),
         )
 
     def char_adjust_capacity(self, request: CharacterizationInput, context):
         in_var = {
-            "platform" : jsonpickle.decode(request.platform)
+            "platform": jsonpickle.decode(request.platform)
         }
         result = building_adjust_capacity(in_var=in_var)
         return CharacterizationOutput(
-            stream=jsonpickle.encode(result, unpicklable=True)
+            stream=json.dumps(result)
         )
 
 
